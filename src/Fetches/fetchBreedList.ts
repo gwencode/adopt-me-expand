944f431b6b25099d8571d21d56fbfc44cc99814c
Gwendal Le Bris <gwen.coding@gmail.com>
2023-11-22 11:15:24 +0100

    Typing fetchBreedList

--- src/Fetches/fetchBreedList.ts
+++ src/Fetches/fetchBreedList.ts
@@ -1,10 +1,14 @@
-const fetchBreedList = async ({ queryKey }) => {
+import { QueryFunction } from "@tanstack/react-query";
+import { Animal, BreedListAPIResponse } from "../APIResponsesTypes";
+
+const fetchBreedList: QueryFunction<
+  BreedListAPIResponse,
+  ["breeds", Animal]
+> = async ({ queryKey }) => {
   const animal = queryKey[1];
   const apiRes = await fetch(
     `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
   );
-
-  if (!animal) return [];
 
   if (!apiRes.ok) {
     throw new Error(`breeds/${animal} fetch not ok`);
