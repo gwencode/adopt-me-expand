6f2f48731df41cdfd3467640b0b775870b5e234d
Gwendal Le Bris <gwen.coding@gmail.com>
2023-12-05 10:54:54 +0100

    Code Splitting with lazy and Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AdoptedPetContext from "./AdoptedPetContext";
-import Details from "./Components/Details";
-import SearchParams from "./Components/SearchParams";
+// import Details from "./Components/Details";
+// import SearchParams from "./Components/SearchParams";
 import { Pet } from "./APIResponsesTypes";
+
+const Details = lazy(() => import("./Components/Details"));
+const SearchParams = lazy(() => import("./Components/SearchParams"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,15 +25,23 @@ const App = () => {
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <AdoptedPetContext.Provider value={adoptedPet}>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/" element={<SearchParams />} />
-            <Route path="/details/:id" element={<Details />} />
-          </Routes>
-        </AdoptedPetContext.Provider>
+        <Suspense
+          fallback={
+            <div className="loading-pane">
+              <h2 className="loader">🐶</h2>
+            </div>
+          }
+        >
+          <AdoptedPetContext.Provider value={adoptedPet}>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/" element={<SearchParams />} />
+              <Route path="/details/:id" element={<Details />} />
+            </Routes>
+          </AdoptedPetContext.Provider>
+        </Suspense>
       </QueryClientProvider>
     </BrowserRouter>
   );
